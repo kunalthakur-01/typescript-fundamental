29433cd8f2166dec5628b1f589bdf751df6dcff5
kunal <13kunalthakur@gmail.com>
2023-02-07 14:23:26 +0530

    reference data types decalarations

--- basic.ts
+++ basic.ts
@@ -8,5 +8,14 @@ let userName: string = 'kunal';
 
 let isLogin: boolean = false;
 
+// reference types
+// arrays, objects
+
+// arrays declaration
+let hoobies: string[];
+
+hoobies = ['cooking', 'playing', 'studying'];
+
+
 
 
